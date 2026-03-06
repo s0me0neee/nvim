c9dc843599079080ad3e81a1830c61268ea433ae
s0me0ne <mym081230@gmail.com>
2026-03-05 23:18:58 -0500

    add cmd runner

--- lua/plugins/image.lua
+++ lua/plugins/image.lua
@@ -1,10 +1,16 @@
 return {
 	{
 		"3rd/image.nvim",
-		lazy = true,
+		lazy = false,
 		build = false, -- so that it doesn't build the rock https://github.com/3rd/image.nvim/issues/91#issuecomment-2453430239
 		opts = {
+			markdown = {
+				enabled = false,
+			},
+			backend = "kitty",
 			processor = "magick_cli",
+			max_width_window_percentage = nil,
+			max_height_window_percentage = 75,
 		},
 	},
 }
